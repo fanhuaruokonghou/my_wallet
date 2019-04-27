d7ef71a57615ea660d61304ff30e8aa867c138ab
fanhua <3184107533@qq.com>
2019-04-27 15:45:22 +0800

    last

--- src/app.js
+++ src/app.js
@@ -111,13 +111,37 @@ App = {
             privateKey = key.derivePath(path).privateKey.toString('hex');
             wallet = new ethers.Wallet(privateKey);
         }
-        App.createWallet(wallet);
+        App.setupWallet(wallet);
         storage.setItem(HDName, mnemonic);
     },
 
+    //切换到HD钱包衍生的第index账户
+    //HDName  钱包名字
+    //password  密码
+    //index  由数字组成的字符串 0 <= index <= 2^32 - 1
+    switchAccounts: function(HDName, password, index){
+        let storage = window.localStorage;
+        let mnemonic = storage.getItem(HDName);
+        let privateKey = null;
+        let wallet = null;
+        let path = "m/44'/60'/0'/0/" + index;
+        if(password === ''){
+            wallet = new ethers.Wallet.fromMnemonic(mnemonic, path);
+        }else{
+            let bip39 = require('bip39');
+            let bip32 = require('bip32');
+            let seed= bip39.mnemonicToSeedSync(mnemonic, password);
+            let key = bip32.fromSeed(seed);
+            privateKey = key.derivePath(path).privateKey.toString('hex');
+            wallet = new ethers.Wallet(privateKey);
+        }
+        App.createWallet(wallet);
+    },
 
-
-    createPrivWallet: function(PrivName, password){  //创建公私钥对钱包
+    //创建公私钥对钱包
+    //privateName  私钥钱包名字
+    //password  密码
+    createPrivWallet: function(privateName, password){
         let randomNumber = ethers.utils.bigNumberify(ethers.utils.randomBytes(32));
         let privateKey = randomNumber._hex;
         let storage = window.localStorage;
@@ -126,10 +150,32 @@ App = {
         let wallet = new ethers.Wallet(privateKey);
         let activeWallet = wallet.connect(provider);
         activeWallet.encrypt(password).then(function(json) {
-            storage.setItem(PrivName, json);
-            console.log(storage.getItem(PrivName));
+            storage.setItem(privateName, json);
+            console.log(storage.getItem(privateName));
         });
         App.setupWallet(wallet);
+    },
+
+    //切换到私钥钱包
+    //privateName  私钥钱包名字
+    //password  密码
+    switchPrivateKey: function(privateName, password){
+        let storage = window.localStorage;
+        let json = storage.getItem(privateName);
+        if (ethers.utils.getJsonWalletAddress(json)) {
+            ethers.Wallet.fromEncryptedJson(json, password).then(function(wallet) {
+                App.setupWallet(wallet);
+            }, function(error) {
+                if (error.message === 'invalid password') {
+                    alert('Wrong Password');
+                } else {
+                    alert('解密账号发生错误...');
+                    console.log(error);
+                }
+            });
+        } else {
+            alert('Unknown JSON wallet format');
+        }
     },
 
     refreshUI: function() {  //刷新以太余额以及代币余额
@@ -139,10 +185,11 @@ App = {
         let nonce = null;
         App.activeWallet.getBalance('pending').then(function(balance) {
             ethBalance = ethers.utils.formatEther(balance, { commify: true });  //以太余额
-        }, function(error) {
+        }, function(tx) {
+            console.log(tx);
+        }),function (error) {
             alert('Error \u2014 ' + error.message);
-            return;
-        });
+        };
 
         App.contract.balanceOf(App.activeWallet.address).then(function(balance){
             tokenBalance = balance;  //代币余额
@@ -230,7 +277,7 @@ App = {
             //  发起交易，前面2个参数是函数的参数，第3个是交易参数
             contractWithSigner.transfer(targetTokenAddress, tokenAmount, {
                 gasLimit: 500000,
-                // 偷懒，直接是用 2gwei
+                // 偷懒，直接使用 2gwei
                 gasPrice: ethers.utils.parseUnits("2", "gwei")
             }).then(function(tx) {
                 console.log(tx);
@@ -252,7 +299,7 @@ App = {
         let contractWithSigner = App.contract.connect(App.activeWallet);
         contractWithSigner.buy({
             gasLimit: 500000,
-            // 偷懒，直接是用 2gwei
+            // 偷懒，直接使用 2gwei
             gasPrice: ethers.utils.parseUnits("2", "gwei"),
             value: ethers.utils.parseEther(ethAmount)
         });
@@ -268,5 +315,121 @@ App = {
             gasPrice: ethers.utils.parseUnits("2", "gwei")
         });
         return App.refreshUI();
+    },
+
+    //在注册的时候生成用于用户协商密钥的mnemonic
+    createECDHNemonic: function(userName, password){
+        let storage = window.localStorage;
+        let mnemonic = ethers.utils.HDNode.entropyToMnemonic(ethers.utils.randomBytes(32));
+        let privateKey = ethers.utils.bigNumberify(ethers.utils.randomBytes(32));
+        let wallet = new ethers.Wallet(privateKey);
+        wallet.encrypt(password).then(function(json) {
+            storage.setItem(userName + 'priv', json);
+            console.log(storage.getItem(privateName));
+        });
+        storage.setItem(userName + 'ECDH', mnemonic);
+    },
+
+
+
+    //密钥协商
+    ECDH: function (userName, index, password) {
+        const crypto = require('crypto');
+        const alice = crypto.createECDH('secp256k1');
+        let privateKeyList = App.decryptFile(userName, index, password);
+        let publicKeyList = new Array(privateKeyList.length);
+        for (let i = 0; i < privateKeyList.length; i++) {
+            alice.setPrivateKey(privateKeyList[i], 'hex');
+            publicKeyList[i] = alice.getPublicKey('hex');
+        }
+        return publicKeyList;
+        // const alice_secret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
+        // const bob_secret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
+    },
+
+    //解密用户购买的文件
+    //userName  用户的唯一名字
+    //index  该用户的交易系列数组
+    //password  解密密钥
+    decryptFile: function (userName, index, password) {
+        let privateKey = null;
+        let privateKeyList = new  Array(index.length);
+        let path = "m/44'/60'/0'/0/";
+        let storage = window.localStorage;
+        let json = storage.getItem(userName + 'priv');
+        if (ethers.utils.getJsonWalletAddress(json)) {
+            ethers.Wallet.fromEncryptedJson(json, password).then(function(wallet) {
+                privateKey = wallet.privateKey
+            }, function(error) {
+                if (error.message === 'invalid password') {
+                    alert('Wrong Password');
+                } else {
+                    alert('解密账号发生错误...');
+                    console.log(error);
+                }
+            });
+        } else {
+            alert('Unknown JSON wallet format');
+        }
+        let bip39 = require('bip39');
+        let bip32 = require('bip32');
+        let seed= bip39.mnemonicToSeedSync(mnemonic, privateKey);
+        let key = bip32.fromSeed(seed);
+        for (let i = 0; i < index.length; i++) {
+            privateKeyList[i] = key.derivePath(path + index[i]).publicKey.toString('hex').substr(2,64);
+        }
+
+        return privateKeyList;
+    },
+
+    //购买非实时数据
+    //seller  卖家地址
+    //fileNumberList 文件序号列表
+    //买家公钥
+    //交易类型  1表示拥有权 2表示所有权
+    //交易代币总额
+    purchaseData: function (seller, fileNumberList, publicKeyCheck, txType, tokenAmount,) {
+
+        let contractWithSigner = App.contract.connect(App.activeWallet);
+        //  发起交易，前面2个参数是函数的参数，第3个是交易参数
+        contractWithSigner.buyData(seller, fileNumberList, publicKeyCheck, txType, tokenAmount, {
+            gasLimit: 500000,
+            // 偷懒，直接使用 2gwei
+            gasPrice: ethers.utils.parseUnits("2", "gwei")
+        }).then(function(tx) {
+            console.log(tx);
+            alert('Success!');
+            return App.refreshUI();
+        }, function(error) {
+            console.log(error);
+            alert('Error \u2014 ' + error.message);
+        });
+    },
+
+    //购买实时数据
+    //publicKeyCheck  买家公钥
+    //IP或者特征值
+    //交易代币总额
+    purchaseRealTimeData: function (publicKeyCheck, ipOrEigenvalues, value) {
+        let contractWithSigner = App.contract.connect(App.activeWallet);
+        contractWithSigner.buyRealTimeData(publicKeyCheck, ipOrEigenvalues, value, {
+            gasLimit: 500000,
+            // 偷懒，直接使用 2gwei
+            gasPrice: ethers.utils.parseUnits("2", "gwei")
+        }).then(function(tx) {
+            console.log(tx);
+            alert('Success!');
+        }, function(error) {
+            console.log(error);
+            alert('Error \u2014 ' + error.message);
+        });
     }
+
+    /*
+    *
+    * administered的功能
+    *
+    * */
+
+
 }
